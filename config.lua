bbf92cafa8dba8d17a266d94d56e673fb189f9e0
JordanBScott <99845881+JordanBScott@users.noreply.github.com>
2023-08-13 13:17:57 -0700

    Update config.lua

--- config.lua
+++ config.lua
@@ -2,21 +2,21 @@ Config = Config or {}
 
 -- **** IMPORTANT ****
 -- UseTarget should only be set to true when using qb-target
-Config.UseTarget = GetConvar('UseTarget', 'false') == 'true'
+Config.UseTarget = true
 
 Config.MinZOffset = 40
 Config.TakeoverPrice = 5000
 Config.TrapHouses = {
-    [1] = { -- Postal 8219
+    [1] = { -- Postal 9156
         coords = {
-            ["enter"] = vector3(-1202.21, -1308.48, 4.92),
-            ["interaction"] = vector3(-1207.63, -1311.75, -32.65),
+            ["enter"] = vector3(405.8, -1751.13, 29.71),
+            ["interaction"] = vector3(400.48, -1754.48, -7.86),
         },
         polyzoneBoxData = {
             ["enter"] = {
-                heading = 290,
-                minZ = 4.0,
-                maxZ = 6.0,
+                heading = 319.33,
+                minZ = 27.0,
+                maxZ = 31.0,
                 debug = false,
                 length = 1,
                 width = 1,
@@ -24,7 +24,47 @@ Config.TrapHouses = {
                 created = false
             },
             ["interaction"] = {
-                heading = 180,
+                heading = 177.96,
+                debug = false,
+                length = 1,
+                width = 1,
+                distance = 1.0,
+                created = false
+            },
+            ["exit"] = {
+                heading = 93.92,
+                debug = false,
+                length = 1,
+                width = 1,
+                distance = 1.0,
+                created = false
+            }
+        },
+        keyholders = {},
+        pincode = 1234,
+        inventory = {},
+        opened = false,
+        takingover = false,
+        money = 0,
+    },
+    [2] = { -- Postal 8108
+        coords = {
+            ["enter"] = vector3(-1177.6, -1073.61, 5.91),
+            ["interaction"] = vector3(-1183.02, -1076.89, -31.66),
+        },
+        polyzoneBoxData = {
+            ["enter"] = {
+                heading = 122.46,
+                minZ = 3.0,
+                maxZ = 7.0,
+                debug = false,
+                length = 1,
+                width = 1,
+                distance = 2.0,
+                created = false
+            },
+            ["interaction"] = {
+                heading = 180.91,
                 debug = false,
                 length = 1,
                 width = 1,
@@ -47,16 +87,16 @@ Config.TrapHouses = {
         takingover = false,
         money = 0,
     },
-    [2] = { -- Postal 1020
+    [3] = { -- Postal 6049
         coords = {
-            ["enter"] = vector3(-39.08, 6420.5, 31.69),
-            ["interaction"] = vector3(-44.4, 6417.22, -5.88),
+            ["enter"] = vector3(-596.83, 851.52, 211.48),
+            ["interaction"] = vector3(-602.2, 848.15, 173.91),
         },
         polyzoneBoxData = {
             ["enter"] = {
-                heading = 44.14,
-                minZ = 29.0,
-                maxZ = 34.0,
+                heading = 220.72,
+                minZ = 209.0,
+                maxZ = 213.0,
                 debug = false,
                 length = 1,
                 width = 1,
@@ -64,7 +104,7 @@ Config.TrapHouses = {
                 created = false
             },
             ["interaction"] = {
-                heading = 185.49,
+                heading = 182.35,
                 debug = false,
                 length = 1,
                 width = 1,
@@ -87,16 +127,16 @@ Config.TrapHouses = {
         takingover = false,
         money = 0,
     },
-    [3] = { -- Postal 2009
+    [4] = { -- Postal 2016
         coords = {
-            ["enter"] = vector3(1651.12, 4830.04, 42.03),
-            ["interaction"] = vector3(1645.76, 4826.76, 4.46),
+            ["enter"] = vector3(1725.47, 4642.4, 43.88),
+            ["interaction"] = vector3(1720.08, 4639.02, 6.31),
         },
         polyzoneBoxData = {
             ["enter"] = {
-                heading = 357.18,
-                minZ = 39.0,
-                maxZ = 45.0,
+                heading = 298.45,
+                minZ = 45.0,
+                maxZ = 41.0,
                 debug = false,
                 length = 1,
                 width = 1,
@@ -104,7 +144,7 @@ Config.TrapHouses = {
                 created = false
             },
             ["interaction"] = {
-                heading = 180.13,
+                heading = 180.85,
                 debug = false,
                 length = 1,
                 width = 1,
@@ -127,46 +167,46 @@ Config.TrapHouses = {
         takingover = false,
         money = 0,
     },
-    [4] = { -- Postal 3031
-        coords = {
-            ["enter"] = vector3(438.48, 3570.54, 33.88),
-            ["interaction"] = vector3(433.15, 3567.26, -3.69),
+    [5] = { -- Postal 1070
+    coords = {
+        ["enter"] = vector3(-229.57, 6445.59, 31.2),
+        ["interaction"] = vector3(-234.9, 6442.31, -6.37),
+    },
+    polyzoneBoxData = {
+        ["enter"] = {
+            heading = 321.52,
+            minZ = 29.0,
+            maxZ = 33.0,
+            debug = false,
+            length = 1,
+            width = 1,
+            distance = 2.0,
+            created = false
         },
-        polyzoneBoxData = {
-            ["enter"] = {
-                heading = 166.82,
-                minZ = 30.0,
-                maxZ = 36.0,
-                debug = false,
-                length = 1,
-                width = 1,
-                distance = 2.0,
-                created = false
-            },
-            ["interaction"] = {
-                heading = 181.46,
-                debug = false,
-                length = 1,
-                width = 1,
-                distance = 1.0,
-                created = false
-            },
-            ["exit"] = {
-                heading = 180,
-                debug = false,
-                length = 1,
-                width = 1,
-                distance = 1.0,
-                created = false
-            }
+        ["interaction"] = {
+            heading = 180.12,
+            debug = false,
+            length = 1,
+            width = 1,
+            distance = 1.0,
+            created = false
         },
-        keyholders = {},
-        pincode = 1234,
-        inventory = {},
-        opened = false,
-        takingover = false,
-        money = 0,
-    }
+        ["exit"] = {
+            heading = 180,
+            debug = false,
+            length = 1,
+            width = 1,
+            distance = 1.0,
+            created = false
+        }
+    },
+    keyholders = {},
+    pincode = 1234,
+    inventory = {},
+    opened = false,
+    takingover = false,
+    money = 0,
+}
 }
 
 Config.AllowedItems = {
